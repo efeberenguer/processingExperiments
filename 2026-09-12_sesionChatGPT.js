6437314311a20fa61a1aa86de419573616376370
efeberenguer <efe.berenguer@gmail.com>
2026-09-12 20:47:44 +0100

    Add bouncing circle animation in sesionChatGPT.js

---
+++ 2026-09-12_sesionChatGPT.js
@@ -0,0 +1,45 @@
+// the code below was developed along ChatGPT
+
+let x = 128;
+let y = 128;
+let velocidadX = -2;
+let velocidadY = -2;
+let diametro = 32;
+//let velocidadOpuesta = velocidad * -1;
+
+function setup() {
+  createCanvas(512, 256);
+}
+
+function draw() {
+  background(64, 20);
+
+  fill(0,0,255);
+  //stroke(0);
+  noStroke();
+  circle(x, y, diametro);
+
+  x = x + velocidadX;
+  y = y + velocidadY;
+
+  if (x > width - (diametro/2)) {
+    x = width - (diametro/2)
+    velocidadX = velocidadX*(-1);
+  }
+
+  if (x < diametro/2) {
+    x = diametro/2
+  velocidadX = velocidadX*(-1);
+  }
+
+  if (y > height - (diametro/2)) {
+    y = height - (diametro/2)
+    velocidadY = velocidadY*(-1);
+  }
+
+  if (y < diametro/2) {
+    y = diametro/2
+  velocidadY = velocidadY*(-1);
+  }
+}
+
